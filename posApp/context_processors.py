3c77dbe40854b01d65dcac7be1a506b0c7d45a76
Alfred Mutua M <43045205+alfredmm@users.noreply.github.com>
2025-08-13 17:55:53 +0300

    Update context_processors.py

--- posApp/context_processors.py
+++ posApp/context_processors.py
@@ -1,6 +1,14 @@
+from .models import UserRole # Import the model
 
-# posApp/context_processors.py
-from .views import get_user_role
+def get_user_role(user):
+    """Get user role"""
+    if user.is_superuser:
+        return 'admin'
+    try:
+        user_role = UserRole.objects.get(user=user)
+        return user_role.role
+    except UserRole.DoesNotExist:
+        return 'waiter'  # default role
 
 def user_role_context(request):
     """Add user role to all templates"""
