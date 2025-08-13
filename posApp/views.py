ea9ebf50e7e03b5597cead91a68c1fe3b5f537a9
Alfred Mutua M <43045205+alfredmm@users.noreply.github.com>
2025-08-13 17:56:58 +0300

    Update views.py

--- posApp/views.py
+++ posApp/views.py
@@ -33,27 +33,27 @@ def is_admin_or_manager(user):
     except UserRole.DoesNotExist:
         return False
 
-def get_user_role(user):
-    """Get user role"""
-    if user.is_superuser:
-        return 'admin'
-    try:
-        user_role = UserRole.objects.get(user=user)
-        return user_role.role
-    except UserRole.DoesNotExist:
-        return 'waiter'  # default role
+# def get_user_role(user):
+#     """Get user role"""
+#     if user.is_superuser:
+#         return 'admin'
+#     try:
+#         user_role = UserRole.objects.get(user=user)
+#         return user_role.role
+#     except UserRole.DoesNotExist:
+#         return 'waiter'  # default role
 
-# Context processor for global template variables
-def user_role_context(request):
-    """Add user role to all templates"""
-    if request.user.is_authenticated:
-        user_role = get_user_role(request.user)
-        return {
-            'user_role': user_role,
-            'show_admin_menu': user_role in ['admin', 'manager'],
-            'show_pos_only': user_role == 'waiter',
-        }
-    return {}
+# # Context processor for global template variables
+# def user_role_context(request):
+#     """Add user role to all templates"""
+#     if request.user.is_authenticated:
+#         user_role = get_user_role(request.user)
+#         return {
+#             'user_role': user_role,
+#             'show_admin_menu': user_role in ['admin', 'manager'],
+#             'show_pos_only': user_role == 'waiter',
+#         }
+#     return {}
 
 # Authentication Views
 def login_user(request):
