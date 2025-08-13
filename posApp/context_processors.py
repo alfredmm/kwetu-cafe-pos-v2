54350ec7c68a13cba94203fa3d0e275681387286
Alfred Mutua M <43045205+alfredmm@users.noreply.github.com>
2025-08-13 17:51:50 +0300

    Update context_processors.py

--- posApp/context_processors.py
+++ posApp/context_processors.py
@@ -1,1 +1,14 @@
 
+# posApp/context_processors.py
+from .views import get_user_role
+
+def user_role_context(request):
+    """Add user role to all templates"""
+    if request.user.is_authenticated:
+        user_role = get_user_role(request.user)
+        return {
+            'user_role': user_role,
+            'show_admin_menu': user_role in ['admin', 'manager'],
+            'show_pos_only': user_role == 'waiter',
+        }
+    return {}
