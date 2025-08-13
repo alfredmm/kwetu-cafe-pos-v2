3d9dd7b4ab714aa98db324250456994c761b86eb
Alfred Mutua M <43045205+alfredmm@users.noreply.github.com>
2025-08-13 19:15:14 +0300

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Add WhiteNoise storage for better static file serving
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Keep this if you have a global static folder
